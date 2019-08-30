d0cf70c1572530b5ef1ac2f4cebffcc2f090b1e6
wesleychipecoIDS <wesley@integrated-digital.com>
2019-08-29 18:49:31 -0700

    Change screen to PureComponent

--- src/BaseballStandings.js
+++ src/BaseballStandings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { connect } from "react-redux";
 import { Row, Rows } from "./components/Row";
@@ -6,66 +6,69 @@ import { Row, Rows } from "./components/Row";
 import { getBaseballStandingsStateSelectors } from "./store/standings/baseballStandingsReducer";
 import { scrapeBaseballStandings } from "./store/standings/baseballStandingsActions";
 
-const BaseballStandings = props => {
-  const handleScrapeRequest = () => {
-    props.scrapeBaseballStandings();
-  };
+class BaseballStandings extends PureComponent {
+  componentDidMount() {
+    this.props.scrapeBaseballStandings();
+  }
 
-  const { navigation, baseballStandings } = props;
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Baseball Standings!</Text>
-      <Text>{props.lastScraped}</Text>
-      <Row
-        data={[
-          "Team Name",
-          "Wins",
-          "Losses",
-          "Ties",
-          "Win %",
-          "H2H Trifecta Points",
-        ]}
-        height={50}
-        totalwidth={700}
-        widthArray={[200, 100, 100, 100, 100, 100]}
-        // flexArray={[2, 1, 1, 1, 1, 1]}
-        rowStyle={{ backgroundColor: "#BEBEBE" }}
-        numberOfLines={2}
-      />
-      <Rows
-        data={baseballStandings}
-        totalheight={500}
-        totalwidth={700}
-        widthArray={[200, 100, 100, 100, 100, 100]}
-        // flexArray={[2, 1, 1, 1, 1, 1]}
-        objectKeys={[
-          "teamName",
-          "wins",
-          "losses",
-          "ties",
-          "winPer",
-          "h2hTrifectaPoints",
-        ]}
-      />
-      <Button title="Scrape!" onPress={handleScrapeRequest} />
-      <Button
-        title="User #1"
-        onPress={() => navigation.navigate("User", { name: "Snufolafakus" })}
-      />
-      <Button
-        title="User #2"
-        onPress={() =>
-          navigation.navigate("User", { name: "Simsalabimbambasaladusaladim" })
-        }
-      />
-      <Button
-        title="Das Modal"
-        onPress={() => navigation.navigate("DasModal")}
-      />
-    </View>
-  );
-};
+  render() {
+    const { navigation, baseballStandings, lastScraped } = this.props;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Baseball Standings!</Text>
+        <Text>{lastScraped}</Text>
+        <Row
+          data={[
+            "Team Name",
+            "Wins",
+            "Losses",
+            "Ties",
+            "Win %",
+            "H2H Trifecta Points",
+          ]}
+          height={50}
+          totalwidth={700}
+          widthArray={[200, 100, 100, 100, 100, 100]}
+          // flexArray={[2, 1, 1, 1, 1, 1]}
+          rowStyle={{ backgroundColor: "#BEBEBE" }}
+          numberOfLines={2}
+        />
+        <Rows
+          data={baseballStandings}
+          totalheight={500}
+          totalwidth={700}
+          widthArray={[200, 100, 100, 100, 100, 100]}
+          // flexArray={[2, 1, 1, 1, 1, 1]}
+          objectKeys={[
+            "teamName",
+            "wins",
+            "losses",
+            "ties",
+            "winPer",
+            "h2hTrifectaPoints",
+          ]}
+        />
+        {/* <Button title="Scrape!" onPress={handleScrapeRequest} /> */}
+        <Button
+          title="User #1"
+          onPress={() => navigation.navigate("User", { name: "Snufolafakus" })}
+        />
+        <Button
+          title="User #2"
+          onPress={() =>
+            navigation.navigate("User", {
+              name: "Simsalabimbambasaladusaladim",
+            })
+          }
+        />
+        <Button
+          title="Das Modal"
+          onPress={() => navigation.navigate("DasModal")}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
