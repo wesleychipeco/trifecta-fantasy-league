55e4fa7df84786f0cf3884affa79f20db40e0c9d
wesleychipecoIDS <wesley@integrated-digital.com>
2019-09-09 00:10:27 -0700

    Basketball standings using redux and mongo for display and sort

--- src/screens/BasketballStandings.js
+++ src/screens/BasketballStandings.js
@@ -2,10 +2,17 @@ import React, { PureComponent } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { connect } from "react-redux";
 import { Row, Rows } from "../components/Row";
-import { Stitch, RemoteMongoClient } from "mongodb-stitch-react-native-sdk";
 
 import { getBasketballStandingsStateSelectors } from "../store/basketballStandings/basketballStandingsReducer";
-import { scrapeBasketballStandings } from "../store/basketballStandings/basketballStandingsActions";
+import {
+  scrapeBasketballStandings,
+  displayBasketballStandings,
+  sortTable,
+} from "../store/basketballStandings/basketballStandingsActions";
+
+import { tableDefaultSortDirections } from "../consts/tableDefaultSortDirections/basketballStandings";
+import { sortArrayBy } from "../utils";
+import { LinkText } from "../components/LinkText";
 
 class BasketballStandings extends PureComponent {
   constructor(props) {
@@ -14,88 +21,174 @@ class BasketballStandings extends PureComponent {
     this.state = {
       currentUser: undefined,
       client: undefined,
-      standings: [],
-      inSeason: false,
-      inPlayoffs: false,
+      inSeason: false, // if inSeason === false -> display from mongo, true -> scrape
+      basketballStandings: {
+        sortedColumn: "totalTrifectaPoints",
+        highToLow: true,
+      },
     };
-    this._loadClient = this._loadClient.bind(this);
   }
 
   componentDidMount() {
-    const { inSeason, inPlayoffs } = this.state;
-    if (!inSeason && !inPlayoffs) {
-      this._loadClient();
-    } else {
+    const { inSeason } = this.state;
+    if (inSeason) {
       this.props.scrapeBasketballStandings();
+    } else {
+      this.props.displayBasketballStandings();
     }
   }
 
-  _loadClient() {
-    const stitchAppClient = Stitch.defaultAppClient;
-    const mongoClient = stitchAppClient.getServiceClient(
-      RemoteMongoClient.factory,
-      "mongodb-atlas"
-    );
-    const db = mongoClient.db("trifecta");
-    const data = db.collection("basketballStandings2019");
+  sortTableByColumn = (tableArray, columnKey) => {
+    const { sortTable } = this.props;
+    const { sortedColumn, highToLow } = this.state.basketballStandings;
 
-    data
-      .find({}, { sort: { totalTrifectaPoints: -1 } })
-      .asArray()
-      .then(docs => {
-        this.setState({
-          standings: docs,
-        });
-      })
-      .catch(err => {
-        console.log("error!", err);
+    const tableArraySorted = [...tableArray];
+
+    if (sortedColumn === columnKey) {
+      this.setState({
+        basketballStandings: {
+          sortedColumn: columnKey,
+          highToLow: !highToLow,
+        },
       });
-  }
+      sortTable(sortArrayBy(tableArraySorted, columnKey, !highToLow));
+    } else {
+      const columnDefaultSortDirection =
+        tableDefaultSortDirections.basketballStandings[columnKey];
+      this.setState({
+        basketballStandings: {
+          sortedColumn: columnKey,
+          highToLow: columnDefaultSortDirection,
+        },
+      });
+      sortTable(
+        sortArrayBy(tableArraySorted, columnKey, columnDefaultSortDirection)
+      );
+    }
+  };
+
+  noop = () => {};
+
+  sortBasketballStandingsByWins = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "wins");
+  };
+
+  sortBasketballStandingsByLosses = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "losses");
+  };
+
+  sortBasketballStandingsByTies = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "ties");
+  };
+
+  sortBasketballStandingsByWinPer = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "winPer");
+  };
+
+  sortBasketballStandingsByTrifectaPoints = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "trifectaPoints");
+  };
+
+  sortBasketballStandingsByPlayoffPoints = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "playoffPoints");
+  };
+
+  sortBasketballStandingsByTotalTrifectaPoints = () => {
+    const { basketballStandings } = this.props;
+    this.sortTableByColumn(basketballStandings, "totalTrifectaPoints");
+  };
+
+  renderHeaderRowColumn = ({ title, onPress }) => {
+    return (
+      <LinkText
+        key={title}
+        title={title}
+        onPress={onPress}
+        textStyles={{ color: "#0041C2" }}
+      />
+    );
+  };
 
   render() {
     const { navigation, basketballStandings, lastScraped } = this.props;
-    const { standings, inSeason, inPlayoffs } = this.state;
+    const { inSeason } = this.state;
 
-    const standingsToDisplay =
-      !inSeason && !inPlayoffs ? standings : basketballStandings;
-
-    if (!standings && !basketballStandings) {
+    if (!basketballStandings) {
       return null;
+    }
+
+    const headerRowMap = [
+      { title: "Team Name", onPress: this.noop },
+      { title: "Wins", onPress: this.sortBasketballStandingsByWins },
+      { title: "Losses", onPress: this.sortBasketballStandingsByLosses },
+      { title: "Ties", onPress: this.sortBasketballStandingsByTies },
+      { title: "Win %", onPress: this.sortBasketballStandingsByWinPer },
+      {
+        title: "Trifecta Points",
+        onPress: this.sortBasketballStandingsByTrifectaPoints,
+      },
+    ];
+    // If not in season, add in playoff points and total trifecta points
+    if (!inSeason) {
+      headerRowMap.push(
+        {
+          title: "Playoff Points",
+          onPress: this.sortBasketballStandingsByPlayoffPoints,
+        },
+        {
+          title: "Total Trifecta Points",
+          onPress: this.sortBasketballStandingsByTotalTrifectaPoints,
+        }
+      );
+    }
+    const headerRow = headerRowMap.map(this.renderHeaderRowColumn);
+
+    const headerRowHeight = 75;
+    const totalHeight = 500;
+    const totalWidth = inSeason ? 700 : 900;
+    const widthArray = [200, 100, 100, 100, 100, 100];
+    const objectKeys = [
+      "teamName",
+      "wins",
+      "losses",
+      "ties",
+      "winPer",
+      "trifectaPoints",
+    ];
+
+    if (!inSeason) {
+      widthArray.push(100, 100);
+      objectKeys.push("playoffPoints", "totalTrifectaPoints");
     }
 
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Basketball Standings!</Text>
-        <Row
-          data={[
-            "Team Name",
-            "Wins",
-            "Losses",
-            "Ties",
-            "Win %",
-            "Trifecta Points",
-            "Playoff Points",
-            "Total Trifecta Points",
-          ]}
-          height={75}
-          widthArray={[200, 100, 100, 100, 100, 100, 100, 100]}
-        />
-        <Rows
-          data={standingsToDisplay}
-          totalheight={500}
-          totalwidth={900}
-          widthArray={[200, 100, 100, 100, 100, 100, 100, 100]}
-          objectKeys={[
-            "teamName",
-            "wins",
-            "losses",
-            "ties",
-            "winPer",
-            "trifectaPoints",
-            "playoffPoints",
-            "totalTrifectaPoints",
-          ]}
-        />
+        <Text>{lastScraped}</Text>
+        <View style={{ alignItems: "center", marginVertical: 10 }}>
+          <Text style={{ alignSelf: "flex-start" }}>Basketball Standings</Text>
+          <Row
+            data={headerRow}
+            height={headerRowHeight}
+            totalWidth={totalWidth}
+            widthArray={widthArray}
+            rowStyle={{ backgroundColor: "#BEBEBE" }}
+          />
+          <Rows
+            data={basketballStandings}
+            totalheight={totalHeight}
+            totalwidth={totalWidth}
+            widthArray={widthArray}
+            objectKeys={objectKeys}
+          />
+        </View>
+
         <Button
           title="Go to Baseball Standings!"
           onPress={() => navigation.navigate("BaseballStandings")}
@@ -130,6 +223,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
   scrapeBasketballStandings,
+  displayBasketballStandings,
+  sortTable,
 };
 
 export default connect(
--- src/store/basketballStandings/basketballStandingsActions.js
+++ src/store/basketballStandings/basketballStandingsActions.js
@@ -1,10 +1,15 @@
+import { Stitch, RemoteMongoClient } from "mongodb-stitch-react-native-sdk";
 import { createAction } from "redux-starter-kit";
 import {
   SCRAPE_BASKETBALL_STANDINGS_START,
   SCRAPE_BASKETBALL_STANDINGS_SUCCESS,
   SCRAPE_BASKETBALL_STANDINGS_FAILED,
+  SAVE_BASKETBALL_STANDINGS,
+  SORT_TABLE,
+  SET_LAST_SCRAPED,
 } from "./basketballStandingsActionTypes";
 import { basketballStandingsScraper } from "../../scrapers/basketballStandings";
+import { format } from "date-fns";
 
 const actions = {
   scrapeBasketballStandingsStart: createAction(
@@ -16,12 +21,15 @@ const actions = {
   scrapeBasketballStandingsFailed: createAction(
     SCRAPE_BASKETBALL_STANDINGS_FAILED
   ),
+  saveBasketballStandings: createAction(SAVE_BASKETBALL_STANDINGS),
+  sortTable: createAction(SORT_TABLE),
+  setLastScrped: createAction(SET_LAST_SCRAPED),
 };
 
 const scrapeBasketballStandings = () => {
   return async function(dispatch) {
     const standings = await basketballStandingsScraper();
-
+    dispatch(actions.setLastScrped(format(new Date(), "M/D/YY h:mm:ss")));
     console.log("standings", standings);
   };
 };
@@ -29,7 +37,32 @@ const scrapeBasketballStandings = () => {
 const displayBasketballStandings = () => {
   return async function(dispatch) {
     console.log("display basketball standings from mongodb");
+    // connect to mongo
+    const stitchAppClient = Stitch.defaultAppClient;
+    const mongoClient = stitchAppClient.getServiceClient(
+      RemoteMongoClient.factory,
+      "mongodb-atlas"
+    );
+    const db = mongoClient.db("trifecta");
+    const data = db.collection("basketballStandings2019");
+
+    // Default sort is totalTrifectaPoints
+    data
+      .find({}, { sort: { totalTrifectaPoints: -1 } })
+      .asArray()
+      .then(docs => {
+        dispatch(actions.saveBasketballStandings(docs));
+      })
+      .catch(err => {
+        console.log("error!", err);
+      });
   };
 };
 
-export { scrapeBasketballStandings, displayBasketballStandings };
+const sortTable = standings => {
+  return async function(dispatch) {
+    dispatch(actions.sortTable(standings));
+  };
+};
+
+export { scrapeBasketballStandings, displayBasketballStandings, sortTable };
