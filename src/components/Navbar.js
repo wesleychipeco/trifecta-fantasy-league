31d0f84d57877b6a65329bac74d9cc220b51e1f0
Wesley Chipeco <wc220u@att.com>
2020-12-28 15:48:07 -0800

    Add 2021 BBall standings. Scrape fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,19 @@ export class Navbar extends PureComponent {
           touchableStyles={styles.button}
           textStyles={styles.text}
         />
+        <MyButton
+          title="2021 Baksetball Standings"
+          onPress={() =>
+            navigation.navigate("BasketballStandings", { year: "2021" })
+          }
+          touchableStyles={{
+            borderWidth: 2,
+            borderColor: "#000000",
+            backgroundColor: "#007FFF",
+            padding: 5,
+          }}
+          textStyles={{ color: "#FFFFFF" }}
+        />
         <StandingsDropdownPost2019 year="2020" navigation={navigation} />
         <MyButton
           title="Trifecta Standings"
